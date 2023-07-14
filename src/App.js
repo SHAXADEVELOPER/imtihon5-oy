fd633e89b4dc65e1c93bd8320ba57bb836a33998
shahzod <shahzodnabiyev19@gmail.com>
2023-07-14 07:41:49 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Main from './component/main/Main';
+import Add from './component/add card/Add';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from './component/profile/Profile';
+import Get_Product from './component/get_product/Get_Product';
+import Login from './component/login/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>   
+        <Routes>
+
+          <Route path='/' exact element={<Login />} />
+          <Route path='/home' element={<Main />}></Route>
+          <Route path='/add' element={<Add />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/product' element={<Get_Product />}></Route>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
